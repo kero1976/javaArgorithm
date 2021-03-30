51edff4eb937db5aaab07ab2e456ff4c9df4d458
けろ <@DESKTOP-RTN05HG.localdomain>
2021-03-31 06:24:16 +0900

    a

---
+++ src/main/java/hanoi/Hanoi.java
@@ -0,0 +1,8 @@
+package hanoi;
+
+public class Hanoi {
+
+	public static void execute(int disk, String src, String dest, String center) {
+		System.out.println("TEST");
+	}
+}
---
+++ src/test/java/hanoi/HanoiTest.java
@@ -0,0 +1,15 @@
+package hanoi;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HanoiTest {
+
+	@Test
+	void ディスク1() {
+		Hanoi.execute(1, "A", "C", "B");
+		fail("未実装");
+
+	}
+}
